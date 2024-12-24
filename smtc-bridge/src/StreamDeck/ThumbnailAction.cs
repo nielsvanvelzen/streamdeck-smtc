9c18d0185510b77fcbbbbe16896c2a0f160213e5
Niels van Velzen <git@ndat.nl>
2024-12-24 21:11:15 +0100

    Use immediate updates

--- smtc-bridge/src/StreamDeck/ThumbnailAction.cs
+++ smtc-bridge/src/StreamDeck/ThumbnailAction.cs
@@ -8,12 +8,11 @@ namespace SmtcBridge.StreamDeck;
 public partial class ThumbnailAction : KeypadBase
 {
 	private readonly MediaSessionListener _mediaSessionListener;
-	private bool _shouldUpdate = true;
 
 	public ThumbnailAction(ISDConnection connection, InitialPayload payload) : base(connection, payload)
 	{
 		var sessionManager = GlobalSystemMediaTransportControlsSessionManager.RequestAsync().GetResults();
-		_mediaSessionListener = new MediaSessionListener(sessionManager, _ => { _shouldUpdate = true; });
+		_mediaSessionListener = new MediaSessionListener(sessionManager, dto => Update(dto).Wait());
 	}
 
 	public override void KeyPressed(KeyPayload payload)
@@ -32,27 +31,30 @@ public partial class ThumbnailAction : KeypadBase
 	{
 	}
 
-	public override async void OnTick()
+	public override void OnTick()
 	{
-		if (_shouldUpdate)
-		{
-			var dto = _mediaSessionListener.CurrentMediaPropertiesDto;
-			if (dto?.ThumbnailContent == null || dto.ThumbnailContentType == null)
-			{
-				await Connection.SetDefaultImageAsync();
-				await Connection.SetStateAsync(0);
-				await Connection.SetTitleAsync(dto?.Title);
-			}
-			else
-			{
-				var mediaType = dto.ThumbnailContentType.Split(',').First();
-				var dataUrl = $"data:{mediaType};base64,{dto.ThumbnailContent}";
-				await Connection.SetImageAsync(dataUrl, 1);
-				await Connection.SetStateAsync(1);
-				await Connection.SetTitleAsync(null);
-			}
+	}
 
-			_shouldUpdate = false;
+	private Task Update(MediaPropertiesDto? dto)
+	{
+		if (dto?.ThumbnailContent == null || dto.ThumbnailContentType == null)
+		{
+			return Task.WhenAll(
+				Connection.SetDefaultImageAsync(),
+				Connection.SetStateAsync(0),
+				Connection.SetTitleAsync(dto?.Title)
+			);
+		}
+		else
+		{
+			var mediaType = dto.ThumbnailContentType.Split(',').First();
+			var dataUrl = $"data:{mediaType};base64,{dto.ThumbnailContent}";
+
+			return Task.WhenAll(
+				Connection.SetImageAsync(dataUrl, 1),
+				Connection.SetStateAsync(1),
+				Connection.SetTitleAsync(null)
+			);
 		}
 	}
 
